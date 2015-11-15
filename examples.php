7023ab57632acf558b826890f2b743f45e231812
sayyn <bluetiqer@hotmail.com>
2015-11-15 12:18:02 +0200

    Update examples.php

--- examples.php
+++ examples.php
@@ -1,6 +1,6 @@
 <?php 
 include ('class.qrcode.php');
-$qr=new CodeQR();//Here are the available values: UTF-8 [Default] Shift_JIS  ISO-8859-1
-$src=$qr->Draw(100,"Hyper Solutions");  // Draw(SIZE,TEXT)
+$qr=new CodeQR('UTF-8');//Here are the available values: UTF-8 [Default] Shift_JIS  ISO-8859-1
+$src=$qr->Draw("Hyper Solutions",100);  // Draw(SIZE,TEXT)
 ?>
 <img src="<?=$src;?>" />
